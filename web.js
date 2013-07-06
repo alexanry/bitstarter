148924780888f3a69ff7c6144a84da557a95734a
alexanry <ubuntu@ip-172-31-25-146.us-west-2.compute.internal>
2013-07-06 14:21:53 +0000

    Updated web.js. Read from file

--- web.js
+++ web.js
@@ -7,11 +7,9 @@ app.get('/', function(request, response) {
 
 var buffer = new Buffer(256);
 
-buffer.write (fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-}));
+buffer.write (fs.readFileSync('index.html'),"utf-8");
 
-  response.send(buffer.toString("utf8"));
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
